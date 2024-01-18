0ed87ace05d8d159576a09be86abd994b05c92a6
Jesse R Reeves <143226100+JesseRReeves@users.noreply.github.com>
2024-01-17 16:44:34 -0800

    test_3

--- Fire_Map/static/js/fire_map.js
+++ Fire_Map/static/js/fire_map.js
@@ -27,53 +27,5 @@ fetch('https://github.com/supvadakkeveetil/Project3_Group1/raw/main/Fire_Map/sta
     // Handle any errors
   });
 
-// Add a click event listener to the map based on year
 
-map.on('click', function(e) {
-    var clickedFeature = e.layer.feature;
-    var year = clickedFeature.properties.fireYear;
-});
-
-    // Update the visualization based on the clicked year
-    geojsonLayer.eachLayer(function(layer) {
-        var featureYear = layer.feature.properties.fireYear;
-
-        if (featureYear === year) {
-            layer.on('click', function(event) {
-                layer.setStyle({
-                  color: 'red',
-                  weight: 2,
-                  opacity: 0.8,
-                  fillColor: 'blue',
-                  fillOpacity: 0.5
-                });
-              });
-            layer.addTo(map);
-        }
-    })
-
-
-
-
-
-
-
-
-    //Process the geojson data and add markers to the map
-
-    L.geojson(data, {
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {
-                radius: feature.properties.fireSize,
-                fillColor: getColor(feature.properties.fireSize),
-                color: '#000',
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            });
-        },
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup('Fire Name: ' + feature.properties.fireName + '<br>Cause of Fire: ' + feature.properites.fireCause + '<br>Fire Size in Acres: ' + feature.properties.fireSize);
-        }
-    }).addTo(map);
 
