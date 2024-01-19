a969f7dfce860e22aa10adf3085a60d2b1dc4ab9
Jesse R Reeves <143226100+JesseRReeves@users.noreply.github.com>
2024-01-19 11:29:13 -0800

    update

--- fire_map.js
+++ fire_map.js
@@ -100,62 +100,13 @@ basemap.addTo(map);
 
 // Retrieve the data from the geojson
 
-fetch('https://raw.githubusercontent.com/supvadakkeveetil/Project3_Group1/main/Fire_Map/static/js/fire_map.js')
+fetch('https://raw.githubusercontent.com/supvadakkeveetil/Project3_Group1/main/Fire_Map/wildfire_geo_CA2.geojson')
   .then(response => response.json())
   .then(data => {
-    // Handle the response data
+    var year = overlays[feature.properties.FIRE_YEAR]
+
   })
   .catch(error => {
     // Handle any errors
   });
-
-// Add a click event listener to the map based on year
-
-map.on('click', function(e) {
-    var clickedFeature = e.layer.feature;
-    var year = clickedFeature.properties.fireYear;
-});
-
-    // Update the visualization based on the clicked year
-    geojsonLayer.eachLayer(function(layer) {
-        var featureYear = layer.feature.properties.fireYear;
-
-        if (featureYear === year) {
-            layer.on('click', function(event) {
-                layer.setStyle({
-                  color: 'red',
-                  weight: 2,
-                  opacity: 0.8,
-                  fillColor: 'blue',
-                  fillOpacity: 0.5
-                });
-              });
-            layer.addTo(map);
-        }
-    })
-
-
-
-
-
-
-
-
-    //Process the geojson data and add markers to the map
-
-    L.geojson(data, {
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {
-                radius: feature.properties.fireSize,
-                fillColor: getColor(feature.properties.fireSize),
-                color: '#000',
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            });
-        },
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup('Fire Name: ' + feature.properties.fireName + '<br>Cause of Fire: ' + feature.properites.fireCause + '<br>Fire Size in Acres: ' + feature.properties.fireSize);
-        }
-    }).addTo(map);
 
